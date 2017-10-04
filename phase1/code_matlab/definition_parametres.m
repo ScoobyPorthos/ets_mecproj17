ea6c79c57b6bc690239edaad1704eb9614f49cf9
rchennou <chennoufraf@hotmail.fr>
2017-10-04 13:42:03 -0400

    Add files via upload

--- phase1/code_matlab/definition_parametres.m
+++ phase1/code_matlab/definition_parametres.m
@@ -16,7 +16,7 @@ D_emp = [13.32 12.14 11.61];
 
 S = mean(S_emp);
 b = mean(b_emp);
-A = (b*b)/S;
+A = b^2/S;
 longueur_totale = mean(longueur_totale_emp);
 D = mean(D_emp);
 
@@ -40,9 +40,16 @@ Wtrap=0.01; % Fraction d'essence dans les conduites
 
 %% ON DETERMINE LE POIDS
 [ Wto, Wfuel, Wempty ] = itertow( 'jet-transport', M_cruise, H_cruise, ...
-    A, TSFC, T_loiter, Wres, Wtrap, Wpayload, range );
+A, TSFC, T_loiter, Wres, Wtrap, Wpayload, range );
 
 Wlanding = Wto-Wfuel;
+
+%% CALCUL CHARGE ALAIRE TO
+WS_TO = Wto/S;
+
+%% CALCUL CHARGE ALAIRE LD
+WS_L = Wlanding/S;
+
 %% ON DETERMINE LA POUSSEE
 T = Wto*0.35;% Cours 3, valeurs typiques longues distances
 
@@ -50,5 +57,23 @@ T = Wto*0.35;% Cours 3, valeurs typiques longues distances
 T_Wto = T/Wto;
 
 
+
+%% AERODYNAMIQUE/PERFORMANCE
+% Finesse max
+F_max = finesse(M_cruise,A)
+
+% Distance d'atterissage fixée 
+SL = 4600; % Optimale : 4600 ft, Réaliste : 5900 ft
+
+% Calcul du CLmax,L = CLmax
+LP = (SL - 400) / 118;
+CLmax = WS_L / LP
+
+% Calcul du CLmax,TO
+CLmaxTO = 0.8 * CLmax
+
+% Calcul de la distance de décollage
+TOP = WS_TO * (1/CLmaxTO) * (1/T_Wto);
+Sto = 20.9*TOP + 87*sqrt(TOP*T_Wto)
 
 
